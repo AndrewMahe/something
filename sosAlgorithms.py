a2a80cd746a945e7278a20e387e5d955b38ff7b7
teogale <33328010+teogale@users.noreply.github.com>
2018-12-22 21:17:46 +0100

    Add files via upload

--- sosAlgorithms.py
+++ sosAlgorithms.py
@@ -1,12 +1,12 @@
-##Une fonction « newBoard(n) » qui retourne une liste à deux dimensions représentant l’état initial d’un plateau de jeu à n lignes et n colonnes.
+
 def caseSizeSelect():
 
     n = 0
     while (n < 5) or (n > 20):
         try:
-            n = int(input("n ="))
+            n = int(input("n = "))
         except ValueError:
-            print("une erreur sur n est survenu")
+            print("Une erreur sur n est survenu")
     return n
 
 
@@ -24,16 +24,17 @@ def playerSelect(n):
     test = True
     while test:
         try:
-            i = int(input("ligne select ="))-1
-            j = int(input("colonne select ="))-1
+            i = int(input("Ligne select = "))-1
+            j = int(input("Colonne select = "))-1
             if (i >= 1 & i < n) and (j >= 1 & j < n):
                 return i, j
         except ValueError:
-            print("une erreur de valeur a été detecter")
+            print("Une erreur de valeur a été detecter")
 
 
-#ne fonction «possibleSquare(board,n,i,j)» qui retourne
-#  True si i et j sont les coordonnées d’une case où un joueur peut poser une lettre, et False sinon.
+# ne fonction «possibleSquare(board,n,i,j)» qui retourne
+#  True si i et j sont les coordonnées d’une case où un joueur
+#  peut poser une lettre, et False sinon.
 
 def possibleSquare(board, n, i, j):
     if board[i][j] == 0:
@@ -41,50 +42,78 @@ def possibleSquare(board, n, i, j):
     else:
         return False
 
+def squareLetter(board,n,i,j):
+    if possibleSquare(board, n, i, j):
+        l = 0
+        while l != "s" and l != "o":
+            try:
+                l = str(input("Selectionner soit s soit o"))
+                print(l)
+            except:
+                print("Une erreur sur la valeur l est intervenu")
+        if l == "s":
+            return 1
 
+        else:
+            return 2
 
-##Une procédure « updateScoreS(board,n,i,j,scores,player,lines) » qui suppose que le joueur player ait posé la lettre « S » sur la case de coordonnées
- ## i et j. Elle recherche alors les éventuels alignements de « SOS » que cela a pu engendrer, et met à jour le score du joueur player et la liste lines.
+# Une procédure « updateScoreS(board,n,i,j,scores,player,lines) »
+# qui suppose que le joueur player ait posé la lettre « S » sur la case de coordonnées
+# i et j. Elle recherche alors les éventuels alignements de « SOS »
+# que cela a pu engendrer, et met à jour le score du joueur player et la liste lines.
 
 def updateScoreS(board, n, i, j, scores, player, lines):
     pass
 
-
-
- ##Une procédure « updateScoreO(board,n,i,j,scores,player,lines) » qui suppose que le joueur player ait posé la lettre « O » sur la case de coordonnées i et j.
-## Elle recherche alors les éventuels alignements de « SOS » que cela a pu engendrer, et met à jour le score du joueur player et la liste lines.
+# Une procédure « updateScoreO(board,n,i,j,scores,player,lines) » qui
+# suppose que le joueur player ait posé la lettre « O » sur la case de coordonnées i et j.
+# Elle recherche alors les éventuels alignements de « SOS » que cela
+#  a pu engendrer, et met à jour le score du joueur player et la liste lines.
 
 def updateScoreO(board,n,i,j,scores,player,lines):
     pass
 
 
-##Une procédure « update(board,n,i,j,l,scores,player,lines) » qui commence par mettre à jour le plateau de jeu en affectant la valeur l à la case
-##de coordonnées i et j. Selon les cas elle appelle ensuite l’une des deux procédures précédentes. Lors de l’appel de cette procédure, la liste lines est vide.
+# Une procédure « update(board,n,i,j,l,scores,player,lines) »
+#  qui commence par mettre à jour le plateau de jeu en affectant la valeur l à la case
+# de coordonnées i et j. Selon les cas elle appelle ensuite l’une
+#  des deux procédures précédentes. Lors de l’appel de cette
+# procédure, la liste lines est vide.
 
 def update(board,n,i,j,l,scores,player,lines):
-    pass
+    board[i][j] = l
 
-##Une fonction « winner(scores) » qui retourne une chaîne de caractère indiquant le résultat de la partie.
+    return board
+
+# Une fonction « winner(scores) » qui retourne une chaîne de
+#  caractère indiquant le résultat de la partie.
 
 def winner(scores):
     pass
-
-
-
 
 
 def main():
 
     n = caseSizeSelect()
     board = newBoard(n)
-    i, j = playerSelect(n)
 
+    scores = 0
+    lines = []
+    player = 0
 
-    possibleSquare(board, n, i, j)
-    print(possibleSquare(board, n, i, j))
+    play = True
+    while play:
 
+        print(board)
+        player += 1
+        print("Tour du joueur",player)
 
+        i, j = playerSelect(n)
+        l = squareLetter(board, n, i, j)
+        update(board, n, i, j, l, scores, player, lines)
 
+        if player == 2:
+            player = 1
 
 
 main()
